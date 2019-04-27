df12be4652e4c4a7999371badaf99ebb5fde60b0
alldifferent <alldifferent@naver.com>
2019-04-27 13:47:30 +0900

    FrameLayout 예시

--- app/src/main/java/com/alldi/a20190427_practice/MainActivity.java
+++ app/src/main/java/com/alldi/a20190427_practice/MainActivity.java
@@ -1,13 +1,20 @@
 package com.alldi.a20190427_practice;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alldi.a20190427_practice.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
     }
 }
